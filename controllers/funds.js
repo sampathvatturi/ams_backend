f5d807da8262d4968349d8ffef0c2e027eea6669
sampathvatturi <65938558+sampathvatturi@users.noreply.github.com>
2022-12-26 17:54:03 +0530

    backend changes

--- controllers/funds.js
+++ controllers/funds.js
@@ -31,62 +31,83 @@ exports.createFund = async (req, res) => {
       // console.log("Fund Res:", result.affectedRows, result.insertId);
       if (!err) {
         if(result.insertId > 0) {
-          const fundId = result.insertId;
-          db.query("INSERT INTO `transactions` SET ? ",
-          [
-            {
-              trsxcn_from: 'Funds',
-              trsxcn_to: data.fund_description,
-              category: 'Funds',
-              remarks: data.fund_description,
-              mode: data.transaction_mode,
-              trsxcn_date: data.fund_released_date,
-              amount: data.fund_value,
-              fund_id: fundId,
-              created_by: data.created_by,
-              updated_by: data.updated_by,
-            },
-          ],
-          (err, result) => {
-            if (!err) {
-              const bAmount = data.transaction_mode === 'banking' ? data.fund_value : 0;
-              const chkAmount =  data.transaction_mode === 'cheque' ? data.fund_value : 0;
-              const cardAmount =  data.transaction_mode === 'card' ? data.fund_value : 0;
-              const cashAmount =  data.transaction_mode === 'cash' ? data.fund_value : 0;
-              const upiAmount =  data.transaction_mode === 'upi' ? data.fund_value : 0;
-              let qry = "update `account_details` SET banking = banking + "+  bAmount + ",";
-              qry = qry + " cheque = cheque + " + chkAmount  + ",";
-              qry = qry + " card = card + " + cardAmount + ",";
-              qry = qry + " cash = cash + " + cashAmount + ",";
-              qry = qry + " upi = upi + " + upiAmount + ",";
-              qry = qry + " upi = upi + " + upiAmount + ",";
-              qry = qry + "updated_date = '" + currdateTime + "',";
-              qry = qry + "updated_by = " + data.updated_by + ",";
-              qry = qry + "total = banking + cheque + card + cash + upi";
-              qry = qry + " where id=1";
-              console.log("Account Qry", qry);
-              db.query(qry,(err, result) => { 
-              console.log(err,result);
-                if(!err){
-                  res.status(200).json({status: "success", message: "Fund and Transaction added successfully"});
-                } else {
-                    res.status(404).json({ status: "failed" });
-                }
-              });
-              // res.status(200).json({status: "success", message: "Fund and Transaction added successfully"});
-            } else { 
-              db.query("delete from funds where fund_id=" + fundId, (err, result) => {
-                if(err){
-                  res.status(404).json({ status: "failed" });
-                } else {
-                  res.status(404).json({ status: "failed" });
-                }
-              })  
-              res.status(404).json({ status: "failed" });
-            }
+          if(data.fund_type == 'state'){
+            id = 14;
+          }else if(data.fund_type == 'central'){
+            id = 15;
           }
-        );
-        }        
+           let details={
+            acc_head : id,
+            type:'funds',
+            remarks:data.fund_description,
+            mode:data.transaction_mode,
+            trsxcn_date:data.fund_released_date,
+            amount:data.fund_value,
+            created_by:data.created_by,
+            ref_acc_head:0
+           }
+           if(createTransaction(details))
+            res.status(200).json({status: "success", message: "Funds added successfully"});
+           else
+            res.status(500).json({status: "failes", message: "Funds adding failed"});
+          }
+          
+        //   const fundId = result.insertId;
+        //   db.query("INSERT INTO `transactions` SET ? ",
+        //   [
+        //     {
+        //       trsxcn_from: 'Funds',
+        //       trsxcn_to: data.fund_description,
+        //       category: 'Funds',
+        //       remarks: data.fund_description,
+        //       mode: data.transaction_mode,
+        //       trsxcn_date: data.fund_released_date,
+        //       amount: data.fund_value,
+        //       fund_id: fundId,
+        //       created_by: data.created_by,
+        //       updated_by: data.updated_by,
+        //     },
+        //   ],
+        //   (err, result) => {
+        //     if (!err) {
+        //       const bAmount = data.transaction_mode === 'banking' ? data.fund_value : 0;
+        //       const chkAmount =  data.transaction_mode === 'cheque' ? data.fund_value : 0;
+        //       const cardAmount =  data.transaction_mode === 'card' ? data.fund_value : 0;
+        //       const cashAmount =  data.transaction_mode === 'cash' ? data.fund_value : 0;
+        //       const upiAmount =  data.transaction_mode === 'upi' ? data.fund_value : 0;
+        //       let qry = "update `account_details` SET banking = banking + "+  bAmount + ",";
+        //       qry = qry + " cheque = cheque + " + chkAmount  + ",";
+        //       qry = qry + " card = card + " + cardAmount + ",";
+        //       qry = qry + " cash = cash + " + cashAmount + ",";
+        //       qry = qry + " upi = upi + " + upiAmount + ",";
+        //       qry = qry + " upi = upi + " + upiAmount + ",";
+        //       qry = qry + "updated_date = '" + currdateTime + "',";
+        //       qry = qry + "updated_by = " + data.updated_by + ",";
+        //       qry = qry + "total = banking + cheque + card + cash + upi";
+        //       qry = qry + " where id=1";
+        //       console.log("Account Qry", qry);
+        //       db.query(qry,(err, result) => { 
+        //       console.log(err,result);
+        //         if(!err){
+        //           res.status(200).json({status: "success", message: "Fund and Transaction added successfully"});
+        //         } else {
+        //             res.status(404).json({ status: "failed" });
+        //         }
+        //       });
+        //       // res.status(200).json({status: "success", message: "Fund and Transaction added successfully"});
+        //     } else { 
+        //       db.query("delete from funds where fund_id=" + fundId, (err, result) => {
+        //         if(err){
+        //           res.status(404).json({ status: "failed" });
+        //         } else {
+        //           res.status(404).json({ status: "failed" });
+        //         }
+        //       })  
+        //       res.status(404).json({ status: "failed" });
+        //     }
+        //   }
+        // );
+        // }        
         // res.status(200).json({status: "success", message: "Fund added successfully",});
       } else res.status(401).json({ status: "failed" });
     }
--- controllers/transactions.js
+++ controllers/transactions.js
@@ -4,15 +4,22 @@ const currdateTime = require('../middleware/currdate');
 
 
 exports.createTransaction = async (req, res) => {
-  data = req.body;
+  if(req.type === 'funds'){
+    data = req;
+  }else{
+    data = req.body;
+  }
+  
   trsxcn_date = currdateTime;
   //acc_head = to,ref_acc_head = from
   credit_query = "insert into transactions set acc_head = "+data.acc_head+",type='credit',remarks='"+data.remarks+"',mode='"+data.mode+"',trsxcn_date='"+trsxcn_date+"',amount="+data.amount+",created_by="+data.created_by+",ref_acc_head="+data.ref_acc_head+" ";
 
   debit_query = "insert into transactions set acc_head = "+data.ref_acc_head+",type='debit',remarks='"+data.remarks+"',mode='"+data.mode+"',trsxcn_date='"+trsxcn_date+"',amount='"+data.amount+"',created_by="+data.created_by+",ref_acc_head="+data.acc_head+" ";
 
-  // console.log(debit_query);
 
+  if(data.type == 'funds'){
+      db.query(credit_query);
+  }else{
     db.query(debit_query,(err,result)=>{
       if(!err){
         db.query(credit_query,(err,result)=>{
@@ -21,6 +28,7 @@ exports.createTransaction = async (req, res) => {
         })
       }else res.status(500).json({status:"failed",message:"Transaction adding failed"});
     })
+  }
   
 };
 
@@ -29,18 +37,27 @@ exports.getTransactions = async (req, res) => {
   start_date = data.start_date.toString().replace(/T/, ' ').replace(/\..+/, '');
   end_date = data.end_date.toString().replace(/T/, ' ').replace(/\..+/, '');
 
+  basequery = "select * from transactions where (trsxcn_date between '"+start_date+"' and '"+end_date+"') ";
+
   if(data.type==="%" && data.acc_head !="%"){
-    query = "select * from transactions where (trsxcn_date between '"+start_date+"' and '"+end_date+"') and (ref_acc_head = "+data.acc_head+") order by trsxcn_date DESC";
+    if(data.acc_head === '14' || data.acc_head === '15'){
+      query = basequery+"and (ref_acc_head = 0 and acc_head = "+data.acc_head+") or (ref_acc_head = "+data.acc_head+") order by trsxcn_date DESC";
+    }else{
+      query = basequery+"and (ref_acc_head = "+data.acc_head+") order by trsxcn_date DESC";
+    }
   }else if(data.type !="%" && data.acc_head !="%"){
-    query = "select * from transactions where (trsxcn_date between '"+start_date+"' and '"+end_date+"') and (ref_acc_head = "+data.acc_head+") and type IN('"+data.type+"') order by trsxcn_date DESC";
-  }else if(data.type==="%" && data.acc_head !="%"){
-    query = "select * from transactions where (trsxcn_date between '"+start_date+"' and '"+end_date+"') and (ref_acc_head = "+data.acc_head+") order by trsxcn_date DESC" ;
+    if(data.acc_head === '14' || data.acc_head === '15'){
+      query = basequery+"and (ref_acc_head = 0 and acc_head = "+data.acc_head+") or (ref_acc_head = "+data.acc_head+") and type IN('"+data.type+"') order by trsxcn_date DESC";
+    }else{
+      query = basequery+"and (ref_acc_head = "+data.acc_head+") and type IN('"+data.type+"') order by trsxcn_date DESC";
+    }
   }else if(data.type !="%" && data.acc_head ==="%"){
     query = "select * from transactions where (trsxcn_date between '"+start_date+"' and '"+end_date+"') and type IN('"+data.type+"') order by trsxcn_date DESC";
   }else{
     query = "select * from transactions where (trsxcn_date between '"+start_date+"' and '"+end_date+"') order by trsxcn_date DESC";
   }
 
+  console.log(query)
   db.query (query,(err, result) => {
     if (!err) {
       if (result.length > 0){
@@ -52,10 +69,9 @@ exports.getTransactions = async (req, res) => {
             trsxcn_date: '',
             debit: '',
             credit: '',
-            ref_acc_head: ''
           }
           tempdata.trsxcn_date = res.trsxcn_date;
-          tempdata.ref_acc_head = res.ref_acc_head;
+          tempdata.ref_acc_head = res.acc_head;
 
           if(res.type == 'debit'){
             tempdata.debit = res.amount;
@@ -66,7 +82,8 @@ exports.getTransactions = async (req, res) => {
 
           tempdata.particulars = {
             ref_acc_head : res.ref_acc_head,
-            remarks : res.remarks
+            remarks : res.remarks,
+            acc_head : res.acc_head
           }
           finaldata.push(tempdata);
         });
